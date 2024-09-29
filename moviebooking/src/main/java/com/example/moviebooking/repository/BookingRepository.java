6c1874639eaddb16d5d35d7de9bdadd24ac8d1c2
JettiGowtham <161497457+JettiGowtham@users.noreply.github.com>
2024-09-29 19:30:52 +0530

    Delete moviebooking directory

--- moviebooking/src/main/java/com/example/moviebooking/repository/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.moviebooking.repository;
-
-import com.example.moviebooking.model.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
--- moviebooking/src/main/java/com/example/moviebooking/repository/ScreeningRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.moviebooking.repository;
-
-import com.example.moviebooking.model.Screening;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScreeningRepository extends JpaRepository<Screening, Long> {
-}
